2426a95573cd324ce360dd072ef811529bba72ab
Ysmael Marques <ysmaelmarkss@gmail.com>
2022-11-22 20:41:25 -0300

    consertando uma besteira

--- src/componentes/contatos.jsx
+++ src/componentes/contatos.jsx
@@ -9,9 +9,9 @@ function contatos() {
     <section className="team-section py-5">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-12 col-md-6">
+          <div className="col-12 col-md-60">
             <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-              <div className="card-body p-4">
+              <div className="card-body p-6">
                 <div className="member-profile position-absolute w-100 text-center">
                   <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
                 </div>
@@ -31,14 +31,12 @@ function contatos() {
 
               <div className="card-footer theme-bg-primary border-0 text-center">
                 <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><svg className="bi" width="1em" height="1em"><img
+                  <li className="list-inline-item "><a target="_blank"><svg className="bi" width="1em" height="1em" ><img
                           src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-                  <li class="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><svg className="bi" width="1em" height="1em"><img
-                          src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
+                  <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img alt=""></img></svg></a></li>
                 </ul>
               
-                <a className="btn btn-secondary" href="https://github.com/rayane1705" _msthash="1116011" _msttexthash="223717" classN="button" id="myBtn"
-                onclick="javascript:alert('Obrigado por entrar em contato conosco!')">Entre em contato</a>
+              <a className="btn btn-secondary" href="https://github.com/PogeYn">Entre em contato</a>
               </div>
             
             </div>
@@ -54,14 +52,14 @@ function contatos() {
 <section className="team-section py-5">
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-12 col-md-6">
+      <div className="col-12 col-md-60">
         <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
           <div className="card-body p-4">
             <div className="member-profile position-absolute w-100 text-center">
               <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
             </div>
             <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
+              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
               <div className="mb-3 text-center">Programador</div>
               <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                 Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
@@ -76,14 +74,12 @@ function contatos() {
 
           <div className="card-footer theme-bg-primary border-0 text-center">
             <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+              <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
             </ul>
            
-            <a className="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="223717" id="myBtn"
-            onclick="javascript:alert('Obrigado por entrar em contato conosco!')">Entre em contato</a>
+            <a className="btn btn-secondary" href="https://github.com/ysmaelmarks?tab=followers" >Entre em contato</a>
           </div>
         
         </div>
@@ -99,14 +95,14 @@ function contatos() {
 <section className="team-section py-5">
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-12 col-md-6">
+      <div className="col-12 col-md-60">
         <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
           <div className="card-body p-4">
             <div className="member-profile position-absolute w-100 text-center">
               <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
             </div>
             <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
+              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Rayane Santos</h5>
               <div className="mb-3 text-center">Programador</div>
               <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                 Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
@@ -121,14 +117,13 @@ function contatos() {
 
           <div className="card-footer theme-bg-primary border-0 text-center">
             <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
             </ul>
            
-            <a className="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="223717" id="myBtn"
-            onclick="javascript:alert('Obrigado por entrar em contato conosco!')">Entre em contato</a>
+            <a className="btn btn-secondary" href="https://github.com/rayane1705">Entre em contato</a>
           </div>
         
         </div>
@@ -144,14 +139,14 @@ function contatos() {
 <section className="team-section py-5">
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-12 col-md-6">
+      <div className="col-12 col-md-60">
         <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
           <div className="card-body p-4">
             <div className="member-profile position-absolute w-100 text-center">
               <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
             </div>
             <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
+              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Larissa</h5>
               <div className="mb-3 text-center">Programador</div>
               <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                 Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
@@ -166,14 +161,13 @@ function contatos() {
 
           <div className="card-footer theme-bg-primary border-0 text-center">
             <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li class="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
             </ul>
            
-            <a className="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="223717" id="myBtn"
-            onclick="javascript:alert('Obrigado por entrar em contato conosco!')">Entre em contato</a>
+            <a className="btn btn-secondary" href="https://github.com/laririn" >Entre em contato</a>
           </div>
         
         </div>
@@ -189,14 +183,14 @@ function contatos() {
 <section className="team-section py-5">
   <div className="container">
     <div className="row justify-content-center">
-      <div className="col-12 col-md-6">
+      <div className="col-12 col-md-60">
         <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
           <div className="card-body p-4">
             <div className="member-profile position-absolute w-100 text-center">
               <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
             </div>
             <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
+              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Jariane</h5>
               <div className="mb-3 text-center">Programador</div>
               <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                 Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
@@ -211,14 +205,13 @@ function contatos() {
 
           <div className="card-footer theme-bg-primary border-0 text-center">
             <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><svg className="bi" width="1em" height="1em"><img
+              <li class="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
                       src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
             </ul>
            
-            <a className="btn btn-secondary" href="#" _msthash="1116011" _msttexthash="223717" id="myBtn"
-            onclick="javascript:alert('Obrigado por entrar em contato conosco!')">Entre em contato</a>
+            <a className="btn btn-secondary" href="https://github.com/Jarianesantos">Entre em contato</a>
           </div>
         
         </div>
@@ -229,9 +222,6 @@ function contatos() {
 </section>
 </main>
 
-
-
-  
 
        </>
 
