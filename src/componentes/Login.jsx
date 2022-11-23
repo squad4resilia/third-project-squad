b5892b11e7e2875ff72472f28c2cfedf7fb5ecdc
Ysmael Marques <ysmaelmarkss@gmail.com>
2022-11-23 19:40:48 -0300

    alterações

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -3,25 +3,27 @@ import '../css/Login.css';
 function Login() {
     return (
         <>
-            <main class="form-signin w-100 m-auto mt-5">
+            <div className="container mt-5">
+            <main class="d-flex flex-wrap justify-content-center">
                 <form>
                     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-                    <div class="form-floating mt-5">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <div class="form-floating mt-5 mb-3">
+                        <input type="text" class="form-control" id="floatingPassword" placeholder="name@example.com" />
                         <label for="floatingInput">Email address</label>
                     </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingPassword" placeholder="Password" />
                         <label for="floatingPassword">Password</label>
                     </div>
-                    <div class="checkbox mb-3">
+                    <div class="checkbox mb-2">
                         <label>
                             <input type="checkbox" value="remember-me" /> Remember me
                         </label>
                     </div>
-                    <button class="w-100 btn btn- btn-primary" type="submit,">Sign in</button>
+                    <button class="w-20 btn btn-lg btn-primary mt-4" type="submit,">Sign in</button>
                 </form>
             </main>
+            </div>
             {/* rodapé, favor não alterar :) */}
             <div className="container">
                 <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
--- src/componentes/contatos.jsx
+++ src/componentes/contatos.jsx
@@ -1,232 +1,156 @@
 import '../css/contato.css'
 
 function contatos() {
-    return (
+  return (
 
-       <>
-
-     <main>
-    <section className="team-section py-5">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-60">
-            <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-              <div className="card-body p-6">
-                <div className="member-profile position-absolute w-100 text-center">
-                  <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
+    <>    
+    <section class="team-section py-5">
+      <div class="container">
+        <div class="row justify-content-center">
+        {/* primeiro card :) */}
+          <div class="col-12 col-md-6">
+            <div class="card border-0 shadow-lg pt-5 my-5 position-relative">
+              <div class="card-body p-4">
+                <div class="member-profile position-absolute w-100 text-center">
+                  <img class="rounded-circle mx-auto d-inline-block shadow-sm"
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
                 </div>
-                <div className="card-text pt-1">
-                  <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
-                  <div className="mb-3 text-center">Programador</div>
+                <div class="card-text pt-1">
+                  <h5 class="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
+                  <div class="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
-                    est, et venenatis nunc. 
-                 
-                  </div>
-
+                    est, et venenatis nunc.</div>
                 </div>
-              
               </div>
-
-              <div className="card-footer theme-bg-primary border-0 text-center">
-                <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item "><a target="_blank"><svg className="bi" width="1em" height="1em" ><img
-                          src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-                  <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img alt=""></img></svg></a></li>
+              <div class="card-footer theme-bg-primary border-0 text-center">
+                <ul class="social-list list-inline mb-0 mx-auto">
+                  <li class="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
+                  <li class="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-github-1.svg" alt="" /></svg></a></li>
                 </ul>
-              
-              <a className="btn btn-secondary" href="https://github.com/PogeYn">Entre em contato</a>
               </div>
-            
             </div>
-         
           </div>
-      </div>
-      </div>
-  </section>
-  </main>
-
-  <main>
-
-<section className="team-section py-5">
-  <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-60">
-        <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-          <div className="card-body p-4">
-            <div className="member-profile position-absolute w-100 text-center">
-              <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
-            </div>
-            <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
-              <div className="mb-3 text-center">Programador</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
-                convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
-                est, et venenatis nunc. 
-             
+          {/* segundo card :) */}
+          <div class="col-12 col-md-6">
+            <div class="card border-0 shadow-lg pt-5 my-5 position-relative">
+              <div class="card-body p-4">
+                <div class="member-profile position-absolute w-100 text-center">
+                  <img class="rounded-circle mx-auto d-inline-block shadow-sm"
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
+                </div>
+                <div class="card-text pt-1">
+                  <h5 class="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
+                  <div class="mb-3 text-center">Programador</div>
+                  <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                    Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
+                    convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
+                    est, et venenatis nunc.</div>
+                </div>
               </div>
-
-            </div>
-          
-          </div>
-
-          <div className="card-footer theme-bg-primary border-0 text-center">
-            <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
-              <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
-            </ul>
-           
-            <a className="btn btn-secondary" href="https://github.com/ysmaelmarks?tab=followers" >Entre em contato</a>
-          </div>
-        
-        </div>
-     
-      </div>
-  </div>
-</div>
-</section>
-</main>
-
-<main>
-
-<section className="team-section py-5">
-  <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-60">
-        <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-          <div className="card-body p-4">
-            <div className="member-profile position-absolute w-100 text-center">
-              <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
-            </div>
-            <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Rayane Santos</h5>
-              <div className="mb-3 text-center">Programador</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
-                convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
-                est, et venenatis nunc. 
-             
+              <div class="card-footer theme-bg-primary border-0 text-center">
+                <ul class="social-list list-inline mb-0 mx-auto">
+                  <li class="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
+                  <li class="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-github-1.svg" alt="" /></svg></a></li>
+                </ul>
               </div>
-
             </div>
-          
           </div>
-
-          <div className="card-footer theme-bg-primary border-0 text-center">
-            <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
-            </ul>
-           
-            <a className="btn btn-secondary" href="https://github.com/rayane1705">Entre em contato</a>
-          </div>
-        
-        </div>
-     
-      </div>
-  </div>
-</div>
-</section>
-</main>
-
-<main>
-
-<section className="team-section py-5">
-  <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-60">
-        <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-          <div className="card-body p-4">
-            <div className="member-profile position-absolute w-100 text-center">
-              <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
-            </div>
-            <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Larissa</h5>
-              <div className="mb-3 text-center">Programador</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
-                convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
-                est, et venenatis nunc. 
-             
+          {/* terceiro card :) */}
+          <div class="col-12 col-md-6">
+            <div class="card border-0 shadow-lg pt-5 my-5 position-relative">
+              <div class="card-body p-4">
+                <div class="member-profile position-absolute w-100 text-center">
+                  <img class="rounded-circle mx-auto d-inline-block shadow-sm"
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
+                </div>
+                <div class="card-text pt-1">
+                  <h5 class="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
+                  <div class="mb-3 text-center">Programador</div>
+                  <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                    Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
+                    convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
+                    est, et venenatis nunc.</div>
+                </div>
               </div>
-
-            </div>
-          
-          </div>
-
-          <div className="card-footer theme-bg-primary border-0 text-center">
-            <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
-            </ul>
-           
-            <a className="btn btn-secondary" href="https://github.com/laririn" >Entre em contato</a>
-          </div>
-        
-        </div>
-     
-      </div>
-  </div>
-</div>
-</section>
-</main>
-
-<main>
-
-<section className="team-section py-5">
-  <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-60">
-        <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-          <div className="card-body p-4">
-            <div className="member-profile position-absolute w-100 text-center">
-              <img className="rounded-circle mx-auto d-inline-block shadow-sm"src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""/>
-            </div>
-            <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Jariane</h5>
-              <div className="mb-3 text-center">Programador</div>
-              <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
-                convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
-                est, et venenatis nunc. 
-             
+              <div class="card-footer theme-bg-primary border-0 text-center">
+                <ul class="social-list list-inline mb-0 mx-auto">
+                  <li class="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
+                  <li class="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-github-1.svg" alt="" /></svg></a></li>
+                </ul>
               </div>
-
             </div>
-          
           </div>
-
-          <div className="card-footer theme-bg-primary border-0 text-center">
-            <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-linkedin-3.svg" alt=""></img></svg></a></li>
-              <li class="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                      src="src/img/svg/iconmonstr-github-1.svg" alt=""></img></svg></a></li>
-            </ul>
-           
-            <a className="btn btn-secondary" href="https://github.com/Jarianesantos">Entre em contato</a>
+          {/* quarto card :) */}
+          <div class="col-12 col-md-6">
+            <div class="card border-0 shadow-lg pt-5 my-5 position-relative">
+              <div class="card-body p-4">
+                <div class="member-profile position-absolute w-100 text-center">
+                  <img class="rounded-circle mx-auto d-inline-block shadow-sm"
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
+                </div>
+                <div class="card-text pt-1">
+                  <h5 class="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
+                  <div class="mb-3 text-center">Programador</div>
+                  <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                    Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
+                    convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
+                    est, et venenatis nunc.</div>
+                </div>
+              </div>
+              <div class="card-footer theme-bg-primary border-0 text-center">
+                <ul class="social-list list-inline mb-0 mx-auto">
+                  <li class="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
+                  <li class="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-github-1.svg" alt="" /></svg></a></li>
+                </ul>
+              </div>
+            </div>
           </div>
-        
-        </div>
-     
-      </div>
-  </div>
-</div>
-</section>
-</main>
+          {/* quinto card :) */}
+          <div class="col-12 col-md-6">
+            <div class="card border-0 shadow-lg pt-5 my-5 position-relative">
+              <div class="card-body p-4">
+                <div class="member-profile position-absolute w-100 text-center">
+                  <img class="rounded-circle mx-auto d-inline-block shadow-sm"
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" />
+                </div>
+                <div class="card-text pt-1">
+                  <h5 class="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
+                  <div class="mb-3 text-center">Programador</div>
+                  <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                    Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
+                    convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
+                    est, et venenatis nunc.</div>
+                </div>
+              </div>
+              <div class="card-footer theme-bg-primary border-0 text-center">
+                <ul class="social-list list-inline mb-0 mx-auto">
+                  <li class="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
+                  <li class="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><svg class="bi" width="1em" height="1em"><img
+                    src="src/img/svg/iconmonstr-github-1.svg" alt="" /></svg></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* fecha todos os cards :) */}
+          </div>
+          </div>
+        </section>
+
+        </>
 
 
-       </>
-
-       
-    )
+        )
 }
 
 
