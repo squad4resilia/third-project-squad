7ed2e5abb08fbaefa2f6482503564d0bc7661456
Rayane <rayanemoraes1705@hotmail.com>
2022-11-24 15:56:41 -0300

    ContatosNovo

--- src/componentes/contatos.jsx
+++ src/componentes/contatos.jsx
@@ -17,19 +17,20 @@ function contatos() {
                 </div>
                 <div className="card-text pt-1">
                   <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Ysmael Marques</h5>
-                  <div className="mb-3 text-center"><strong>Programador</strong></div>
+                  <div className="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
                     est, et venenatis nunc.</div>
                 </div>
               </div>
-              <div className="card-footer theme-bg-primary border-0 text-center">
-                <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img alt="" /></svg></a></li>
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+              <div className="">
+              <ul className="social-list list-inline mb-0 mx-auto">
+              <li className="list-inline-item"><a href="https://www.github.com/ysmaelmarks" target="_blank"><img className='img'
+                    src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></a></li>
+                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/ysmael-marques/" target="_blank"><img className='img' id='img'
+                    src="https://www.clipartmax.com/png/middle/304-3041379_linkedin-with-circle-comments-transparent-background-website-icon.png" alt="" /></a></li>
                 </ul>
-                <a className="btn btn-secondary" href="https://github.com/ysmaelmarks" _msthash="1116011" _msttexthash="223717" id="myBtn">Entre em contato</a>
               </div>
             </div>
           </div>
@@ -43,20 +44,20 @@ function contatos() {
                 </div>
                 <div className="card-text pt-1">
                   <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Yan Alves</h5>
-                  <div className="mb-3 text-center"><strong>Programador</strong></div>
+                  <div className="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
                     est, et venenatis nunc.</div>
                 </div>
               </div>
-              <div className="card-footer theme-bg-primary border-0 text-center">
-                <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"><img
-                    src="src/img/svg/iconmonstr-linkedin-3.svg" alt="" /></svg></a></li>
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+              <div className="">
+              <ul className="social-list list-inline mb-0 mx-auto">
+              <li className="list-inline-item"><a href="https://www.linkedin.com/in/yan-alves-7b953123a/" target="_blank"><img className='img'
+                    src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></a></li>
+                  <li className="list-inline-item"><a href="https://github.com/PogeYn" target="_blank"><img className='img' id='img'
+                    src="https://www.clipartmax.com/png/middle/304-3041379_linkedin-with-circle-comments-transparent-background-website-icon.png" alt="" /></a></li>
                 </ul>
-                <a className="btn btn-secondary" href="https://github.com/PogeYn" _msthash="1116011" _msttexthash="223717" id="myBtn">Entre em contato</a>
               </div>
             </div>
           </div>
@@ -66,23 +67,24 @@ function contatos() {
               <div className="card-body p-4">
                 <div className="member-profile position-absolute w-100 text-center">
                   <img className="rounded-circle mx-auto d-inline-block shadow-sm"
-                    src="https://i.pinimg.com/474x/41/dc/1e/41dc1ea42e8f371a21abaad5f16f3231.jpg" alt="" />
+                    src="https://th.bing.com/th/id/OIP.bDLkAdcxKVUsvbZ_VVZ05QAAAA?pid=ImgDet&rs=1" alt="" />
                 </div>
                 <div className="card-text pt-1">
                   <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Rayane Santos</h5>
-                  <div className="mb-3 text-center"><strong>Programador</strong></div>
+                  <div className="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
                     est, et venenatis nunc.</div>
                 </div>
               </div>
-              <div className="card-footer theme-bg-primary border-0 text-center">
-                <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+              <div className="">
+              <ul className="social-list list-inline mb-0 mx-auto">
+                 <li className="list-inline-item"><a href="https://github.com/rayane1705" target="_blank"><img className='img'
+                    src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></a></li>
+                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/rayane-santos-1915a024b/" target="_blank"><img className='img' id='img'
+                    src="https://www.clipartmax.com/png/middle/304-3041379_linkedin-with-circle-comments-transparent-background-website-icon.png" alt="" /></a></li>
                 </ul>
-                <a className="btn btn-secondary" href="https://github.com/rayane1705" _msthash="1116011" _msttexthash="223717" id="myBtn">Entre em contato</a>
               </div>
             </div>
           </div>
@@ -95,20 +97,21 @@ function contatos() {
                     src="https://bordadopontocruz.com/wp-content/uploads/2019/06/Princesas-Disney-Pop-Art-3.png" alt="" />
                 </div>
                 <div className="card-text pt-1">
-                  <h5 className="member-name mb-0 text-center text-primary font-weight-bold" id='btnn'>Jariane Santos</h5>
-                  <div className="mb-3 text-center"><strong>Programador</strong></div>
+                  <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Jariane Santos</h5>
+                  <div className="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
                     est, et venenatis nunc.</div>
                 </div>
               </div>
-              <div className="card-footer theme-bg-primary border-0 text-center">
+              <div className="">
                 <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
-                  <li className="list-inline-item"><a  target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+                <li className="list-inline-item"><a href="https://github.com/Jarianesantos" target="_blank"><img className='img'
+                    src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></a></li>
+                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/jariane-santos-b2921a1b2/" target="_blank"><img className='img' id='img'
+                    src="https://www.clipartmax.com/png/middle/304-3041379_linkedin-with-circle-comments-transparent-background-website-icon.png" alt="" /></a></li>
                 </ul>
-                <a className="btn btn-secondary" href="https://github.com/Jarianesantos" _msthash="1116011" _msttexthash="223717" id="myBtn">Entre em contato</a>
               </div>
             </div>
           </div>
@@ -121,20 +124,21 @@ function contatos() {
                     src="https://bordadopontocruz.com/wp-content/uploads/2019/06/Princesas-Disney-Pop-Art-2.png" alt="" />
                 </div>
                 <div className="card-text pt-1">
-                  <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Larissa</h5>
-                  <div className="mb-3 text-center"><strong>Programador</strong></div>
+                  <h5 className="member-name mb-0 text-center text-primary font-weight-bold">Larissa Martins</h5>
+                  <div className="mb-3 text-center">Programador</div>
                   <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Vivamus eget eros vestibulum, accumsan ante viverra, condimentum tellus. Curabitur pellentesque
                     convallis purus non ornare. Donec bibendum sed purus dignissim rutrum. Maecenas bibendum feugiat
                     est, et venenatis nunc.</div>
                 </div>
               </div>
-              <div className="card-footer theme-bg-primary border-0 text-center">
+              <div className="">
                 <ul className="social-list list-inline mb-0 mx-auto">
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
-                  <li className="list-inline-item"><a target="_blank"><svg className="bi" width="1em" height="1em"></svg></a></li>
+                  <li className="list-inline-item"><a href="https://github.com/laririn" target="_blank"><img className='img'
+                    src="https://pngimg.com/uploads/github/github_PNG40.png" alt="" /></a></li>
+                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/larissa98pereira/" target="_blank"><img className='img' id='img'
+                    src="https://www.clipartmax.com/png/middle/304-3041379_linkedin-with-circle-comments-transparent-background-website-icon.png" alt="" /></a></li>
                 </ul>
-                <a className="btn btn-secondary" href="https://github.com/laririn" _msthash="1116011" _msttexthash="223717" id="myBtn">Entre em contato</a>
               </div>
             </div>
           </div>
