71730d8f79b0f7c456b0856222241714b997198e
Laririn <larissa98pereira@gmail.com>
2022-11-24 23:21:15 -0300

    atualização do componente do index

--- src/componentes/Index.jsx
+++ src/componentes/Index.jsx
@@ -18,10 +18,10 @@ function Index() {
   return (
     <>
       {/* Jumbotron */}
-      <div className="p-5 mb-4 myBackground rounded-3">
-        <div className="container-fluid py-5 text-center">
-          <h1 className="display-5 fw-bold text-dark ">NOME DA FERRAMENTA</h1>
-          <p className=" fs-4  text-white text-justify">Sabemos o que você precisa para o seu negócio!<br /> Por isso desenvolvemos esta ferramenta para facilitar seus processos.</p>
+      <div className="p-5 mb-4 mt-5 myBackground rounded-3" id="classedesgraçada">
+        <div className="container-fluid py-5 text-center ">
+          <h1 className="display-5 fw-bold text-dark mybackground2" id="classedesgraçada2">Squadx</h1>
+          <p className=" fs-4  text-white text-justify mybackground2" id="classedesgraçada3">Sabemos o que você precisa para o seu negócio!<br /> Por isso desenvolvemos esta ferramenta para facilitar seus processos.</p>
         </div>
       </div>
       {/* hanging icons */}
@@ -29,39 +29,21 @@ function Index() {
         <h2 className="pb-2 border-bottom">FUNCIONALIDADES</h2>
         <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
           <div className="col d-flex align-items-start">
-            <div className="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-              <svg className="bi" width="1em" height="1em"><use xlinkHref="#toggles2" /></svg>
-            </div>
             <div>
               <h3 className="fs-2">Cadastrar</h3>
-              <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-              <a href="#" className="btn btn-primary">
-                Primary button
-              </a>
+              <p>Preenchendo os campos necessários para o cadastro será possível armazenar o item num banco de dados automatizado.</p>
             </div>
           </div>
           <div className="col d-flex align-items-start">
-            <div className="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-              <svg className="bi" width="1em" height="1em"><use xlinkHref="#cpu-fill" /></svg>
-            </div>
             <div>
               <h3 className="fs-2">Buscar</h3>
-              <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-              <a href="#" className="btn btn-primary">
-                Primary button
-              </a>
+              <p>Após cadastrado, o item poderá ser consultado preenchendo o nome imposto para o mesmo.</p>
             </div>
           </div>
           <div className="col d-flex align-items-start">
-            <div className="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-              <svg className="bi" width="1em" height="1em"><use xlinkHref="#tools" /></svg>
-            </div>
             <div>
               <h3 className="fs-2">Remover</h3>
-              <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-              <a href="#" className="btn btn-primary">
-                Primary button
-              </a>
+              <p>Conforme cadastrado o item, será possível buscar pelo nome atribuído e remove-lo.</p>
             </div>
           </div>
         </div>
@@ -71,7 +53,7 @@ function Index() {
       <div className="p-5 mb-4 bg-dark rounded-3">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold text-white">A FERRAMENTA ERP IDEAL PARA O SEU NEGÓCIO!</h1>
-          <p className="fs-4 text-white">_____ é uma ferramenta software de gestão de estoque que interliga todos os dados e processos da sua organização em um único sistema!</p>
+          <p className="fs-4 text-white">Squadx é uma ferramenta software de gestão de estoque que interliga todos os dados e processos da sua organização em um único sistema!</p>
           <button className="btn btn-primary btn-lg" type="button">Entre em contato!</button>
         </div>
       </div>
@@ -83,7 +65,7 @@ function Index() {
             <div className="col">
               <div className="card mb-4 rounded-3 shadow-sm">
                 <div className="card-header py-3">
-                  <h4 className="my-0 fw-normal">Free</h4>
+                  <h4 className="my-0 fw-normal">Grátis</h4>
                 </div>
                 <div className="card-body">
                   <h1 className="card-title pricing-card-title">$0<small className="text-muted fw-light">/mo</small></h1>
@@ -93,7 +75,7 @@ function Index() {
                     <li>Email support</li>
                     <li>Help center access</li>
                   </ul>
-                  <button type="button" className="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+                  <button type="button" className="w-100 btn btn-lg btn-primary">Cadastre-se</button>
                 </div>
               </div>
             </div>
@@ -112,7 +94,7 @@ function Index() {
                     <li>Priority email support</li>
                     <li>Help center access</li>
                   </ul>
-                  <button type="button" className="w-100 btn btn-lg btn-primary">Get started</button>
+                  <button type="button" className="w-100 btn btn-lg btn-primary">Adquira</button>
                 </div>
               </div>
             </div>
@@ -121,7 +103,7 @@ function Index() {
             <div className="col">
               <div className="card mb-4 rounded-3 shadow-sm border-primary">
                 <div className="card-header py-3 text-bg-primary border-primary">
-                  <h4 className="my-0 fw-normal">Enterprise</h4>
+                  <h4 className="my-0 fw-normal">Empresa</h4>
                 </div>
                 <div className="card-body">
                   <h1 className="card-title pricing-card-title">$29<small className="text-muted fw-light">/mo</small></h1>
@@ -131,7 +113,7 @@ function Index() {
                     <li>Phone and email support</li>
                     <li>Help center access</li>
                   </ul>
-                  <button type="button" className="w-100 btn btn-lg btn-primary">Contact us</button>
+                  <button type="button" className="w-100 btn btn-lg btn-primary">Contate-nos</button>
                 </div>
               </div>
             </div>
@@ -141,7 +123,7 @@ function Index() {
 
       {/* rodapé, favor não alterar :) */}
       <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top sticky-bottom backgroundfooter">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top sticky-bottom">
           <p className="col mb-0 text-muted">© MMXXII</p>
         </footer>
       </div>
